19b4be2f22fb1f2773e510605ec624567591af3a
Omar Abdellatif <oahabdellatif@gmail.com>
2024-09-26 01:45:51 -0400

    Update home.nix

--- home.nix
+++ home.nix
@@ -28,6 +28,12 @@
 
   fonts.fontconfig.enable = true;
 
+  programs.git = {
+    enable = true;
+    userName = "Omar Abdellatif";
+    userEmail = "oahabdellatif@gmail.com";
+  };
+
   #home.file.".emacs.d" = {
   #  source = ./emacs.d;
   #  recursive = true;
